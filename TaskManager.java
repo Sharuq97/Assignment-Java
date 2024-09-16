5c985d35374be586aa2ede8e5cec51e3fa5c2531
Sharuq97 <sharuqqqqq97@gmail.com>
2024-09-16 10:17:33 +0800

    Update TaskManager.java
    finallll

--- TaskManager.java
+++ TaskManager.java
@@ -1,6 +1,6 @@
-// Interface defining CRUD operations (Requirement: Must have at least one interface or abstract class)
+// Interface defining CRUD operations 
 public interface TaskManager {
-    // Methods accepting multiple parameters (Requirement: At least 2 methods with parameters)
+    // Methods accepting multiple parameters 
     void createTask(int id, String description, boolean isComplete, int urgency, double estimatedTime);
     Task readTask(int id);
     void updateTask(int id, String description, boolean isComplete, int urgency, double estimatedTime);
